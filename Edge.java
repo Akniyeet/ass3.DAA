a3da7807968968a61e8122d6e1772214c5a3e038
Akniyeet <akniyet.maksatova@mail.ru>
2025-10-27 23:32:04 +0500

    Create Edge.java

---
+++ Edge.java
@@ -0,0 +1,10 @@
+class Edge {
+    String from;
+    String to;
+    int weight;
+
+    @Override
+    public String toString() {
+        return "From: " + from + " To: " + to + " Weight: " + weight;
+    }
+}
