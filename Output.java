2a47aeff56b0612d1cbf13b5a2222ff842c74274
Akniyeet <akniyet.maksatova@mail.ru>
2025-10-27 23:34:47 +0500

    Create Output.java

---
+++ Output.java
@@ -0,0 +1,9 @@
+import java.util.List;  // Add this import at the top
+
+class Output {
+    List<Result> results;
+
+    public Output(List<Result> results) {
+        this.results = results;
+    }
+}
